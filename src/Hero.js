830eb5a8ca1688d2eda2dfd26986b79d83f19a75
Sadia <sadiashah059@gmail.com>
2022-09-16 13:22:48 -0200

    nesting, variable

--- src/Hero.js
+++ src/Hero.js
@@ -1,21 +1,35 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const color = "red"
 
-const Wrapper = styled.section`
-
-.btn {
+const Button = styled.button`
+    border:2px solid black;
+    padding:20px;
     background:black;
     color:white;
-    padding:20px;
-}
+    border-radius:10px;
+
+    &:hover{
+        color:black;
+        background:white;
+    }
+
+    span{
+        color:${color};
+        &:hover{
+            color:blue;
+        }
+    }
+
 `
 function Hero() {
 
   return (
-    <Wrapper>
-            <button className='btn'>Hello World</button>
-    </Wrapper>
+        <div>
+
+                <Button>Hello World<span> I am span</span> </Button>
+        </div>   
   )
 }
 
